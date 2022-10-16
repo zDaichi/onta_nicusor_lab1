724753c3f132e7932790506970303c62367d741a
Nicusor <Nicusor@DESKTOP-HURLH59>
2022-10-16 08:09:09 -0700

    Add project files.

---
+++ onta_nicusor_lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace onta_nicusor_lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
